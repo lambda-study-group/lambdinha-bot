cfec781cf87470abcbd46ba40ca0582b801ac68d
Allan Araujo <allanxp4@gmail.com>
2017-11-24 00:02:20 -0200

    dependencies

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,7 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :nadia,
+    token:  {:system, "TELEGRAM_API_TOKEN", ""}
+
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule LambdaStudyGroupBot.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      [applications: [:nadia]]      
       extra_applications: [:logger]
     ]
   end
